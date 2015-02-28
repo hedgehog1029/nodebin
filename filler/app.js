03209a646c9ce3effaa321297624dec138a053f7
hedgehog1029 <omgredstone@gmail.com>
2015-02-28 12:02:04 +0000

    trying to make a better looking file server

--- filler/app.js
+++ filler/app.js
@@ -11,12 +11,22 @@ var log = function(msg) {
 app.get("/", function(req, res) {
     fs.readdir(__dirname, function(err, files) {
         if (err) log(err);
-
+        res.set("text/html");
+        res.send("<html><head><link rel='stylesheet' href='style.css' /></head><body>");
+        res.send("<h1>Kurisu~ Files</h1>");
+        for (var file in files) {
+            res.send("<hr><a href='/download?file=" + files[file] + "'>" + files[file] + "</a>");
+        }
+        res.send("</body></html>");
     });
+});
+
+app.get("/download", function(req, res) {
+    res.download("./" + req.query.file);
 });
 
 app.get("/irc", function(req, res) {
     res.redirect("http://dev.kurisubrooks.com:8080/");
 });
 
-app.listen(8080);
+app.listen(80);
