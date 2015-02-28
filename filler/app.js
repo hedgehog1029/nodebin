6c2bfe5e9f1a6d0515e462767de4b7d5bee9a406
hedgehog1029 <omgredstone@gmail.com>
2015-02-28 18:18:37 +0000

    root check

--- filler/app.js
+++ filler/app.js
@@ -18,7 +18,7 @@ app.get("/", function(req, res) {
 
     dir(currentdir, function(html) {
         res.send(html);
-    });
+    }, true);
 });
 
 app.get("/download", function(req, res) {
@@ -31,15 +31,22 @@ app.get("/folder", function(req, res) {
         res.send(html);
         previousdir = currentdir;
         currentdir = currentdir + "/" + req.query.dir;
-    });
+    }, false);
 });
 
 app.get("/up", function(req, res) {
+    var isRoot;
+    if (previousdir == __dirname) {
+        isRoot = true;
+    } else {
+        isRoot = false;
+    }
+
     dir(previousdir, function(html) {
         currentdir = previousdir;
         previousdir = currentdir.substring(0, currentdir.lastIndexOf("/"));
         res.send(html);
-    });
+    }, false);
 });
 
 app.get("/irc", function(req, res) {
@@ -53,10 +60,12 @@ log("started server");
 
 function dir(name, callback) {
     var html;
-    fs.readdir(name, function(err, files) {
+    fs.readdir(name, function(err, files, isRoot) {
         if (err) log(err);
         html = '<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="style.css"><link href="Caramel.css" rel="stylesheet"></head><body><div class="container">' + "<h1>/public_html</h1><hr>";
-        html = html + "<span id='folder'><a href='/up'>..</a></span><br>";
+        if (!isRoot) {
+            html = html + "<span id='folder'><a href='/up'>..</a></span><br>";
+        }
         for (var folder in files) {
             if (files[folder].indexOf(".") == -1) {
                 html = html + "<span id='folder'><i class='fa fa-fw fa-folder'></i>&nbsp;<a href='/folder?dir=" + files[folder] + "'>" + files[folder] + "</a></span><br>";
