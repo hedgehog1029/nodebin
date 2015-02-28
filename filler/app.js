8c00640dbb12f7bfe3e6158689c2c0b1c9f9e13f
hedgehog1029 <omgredstone@gmail.com>
2015-02-28 12:20:01 +0000

    Folder tracking

--- filler/app.js
+++ filler/app.js
@@ -11,9 +11,14 @@ var log = function(msg) {
 app.get("/", function(req, res) {
     fs.readdir(__dirname, function(err, files) {
         if (err) log(err);
-        var html = "<html><head><link rel='stylesheet' href='style.css' /></head><body><h1>Kurisu's Files</h1><hr>";
+        var html = "<html><head><link rel='stylesheet' href='style.css'/><link href='Caramel.css' rel='stylesheet'/></head><body><h1>Kurisu's Files</h1><hr>";
+        for (var folder in files) {
+            if (folder.indexOf(".") == -1) {
+                html = html + "<span id='folder'><i class='fa fa-folder'></i>&nbsp;<a href='/folder?f=" + files[folder] + "'>" + files[folder] + "</a></span><br>";
+            }
+        }
         for (var file in files) {
-            html = html + "<a href='/download?file=" + files[file] + "'>" + files[file] + "</a><br>";
+            html = html + "<span id='file'><i class='fa fa-file'></i>&nbsp;<a href='/download?file=" + files[file] + "'>" + files[file] + "</a></span><br>";
         }
         html = html + "</body></html>";
         res.send(html);
