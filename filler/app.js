eb8fe8e4a412aaaf75546f2a0dab20e5cd34bcfc
hedgehog1029 <omgredstone@gmail.com>
2015-02-28 12:25:46 +0000

    Fixed stylesheet & duplicating

--- filler/app.js
+++ filler/app.js
@@ -11,14 +11,16 @@ var log = function(msg) {
 app.get("/", function(req, res) {
     fs.readdir(__dirname, function(err, files) {
         if (err) log(err);
-        var html = "<html><head><link rel='stylesheet' href='style.css'/><link href='Caramel.css' rel='stylesheet'/></head><body><h1>Kurisu's Files</h1><hr>";
+        var html = "<html><head><link rel='stylesheet' href='https://raw.githubusercontent.com/hedgehog1029/nodebin/master/filler/style.css'/><link href='https://raw.githubusercontent.com/hedgehog1029/nodebin/master/filler/Caramel.css' rel='stylesheet'/></head><body><h1>Kurisu's Files</h1><hr>";
         for (var folder in files) {
             if (files[folder].indexOf(".") == -1) {
                 html = html + "<span id='folder'><i class='fa fa-folder'></i>&nbsp;<a href='/folder?f=" + files[folder] + "'>" + files[folder] + "</a></span><br>";
             }
         }
         for (var file in files) {
-            html = html + "<span id='file'><i class='fa fa-file'></i>&nbsp;<a href='/download?file=" + files[file] + "'>" + files[file] + "</a></span><br>";
+            if (files[file].indexOf(".") != -1) {
+                html = html + "<span id='file'><i class='fa fa-file'></i>&nbsp;<a href='/download?file=" + files[file] + "'>" + files[file] + "</a></span><br>";
+            }
         }
         html = html + "</body></html>";
         res.send(html);
