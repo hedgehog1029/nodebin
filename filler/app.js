ded2d76c68fb0dc5897a3dbc145a2c128fb78523
hedgehog1029 <omgredstone@gmail.com>
2015-02-28 11:44:04 +0000

    Added a simple Express routing app.

---
+++ filler/app.js
@@ -0,0 +1,22 @@
+var express = require("express"),
+    colors = require("colors"),
+    fs = require("fs");
+
+var app = express();
+
+var log = function(msg) {
+    console.log("filler >".red + msg);
+}
+
+app.get("/", function(req, res) {
+    fs.readdir(__dirname, function(err, files) {
+        if (err) log(err) return;
+
+    });
+});
+
+app.get("/irc", function(req, res) {
+    res.redirect("http://dev.kurisubrooks.com:8080/");
+});
+
+app.listen(8080);
