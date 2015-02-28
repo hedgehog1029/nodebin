46f28ef3b724d6dc2d579926e043ce329a77a82d
Penagwin <langpenguin@gmail.com>
2015-02-28 08:21:51 -0500

    App not server

--- filler/app.js
+++ filler/app.js
@@ -35,6 +35,6 @@ app.get("/irc", function(req, res) {
     res.redirect("http://dev.kurisubrooks.com:8080/");
 });
 
-server.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/'));
 
 app.listen(8080);
